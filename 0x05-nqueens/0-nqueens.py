f3863d6010bfdb9e32c330e977e3704f86b3c5e0
heyelizabeth <helloaleeyah@gmail.com>
2024-09-22 06:02:29 -0700

    updated

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,91 +1,56 @@
 #!/usr/bin/python3
-'''N Queens Challenge'''
-
 import sys
 
+# Handle input validation
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
 
-    solutions = []
-    placed_queens = []  # coordinates format [row, column]
-    stop = False
-    r = 0
-    c = 0
+# Define the function to check if it's safe to place a queen
+def is_safe(board, row, col):
+    """Check if it's safe to place a queen at board[row][col]"""
+    # Check column
+    for i in range(row):
+        if board[i] == col:
+            return False
 
-    # iterate thru rows
-    while r < n:
-        goback = False
-        # iterate thru columns
-        while c < n:
-            # check is current column is safe
-            safe = True
-            for cord in placed_queens:
-                col = cord[1]
-                if(col == c or col + (r-cord[0]) == c or
-                        col - (r-cord[0]) == c):
-                    safe = False
-                    break
+    # Check diagonals
+    for i in range(row):
+        if abs(board[i] - col) == abs(i - row):
+            return False
 
-            if not safe:
-                if c == n - 1:
-                    goback = True
-                    break
-                c += 1
-                continue
+    return True
 
-            # place queen
-            cords = [r, c]
-            placed_queens.append(cords)
-            # if last row, append solution and reset all to last unfinished row
-            # and last safe column in that row
-            if r == n - 1:
-                solutions.append(placed_queens[:])
-                for cord in placed_queens:
-                    if cord[1] < n - 1:
-                        r = cord[0]
-                        c = cord[1]
-                for i in range(n - r):
-                    placed_queens.pop()
-                if r == n - 1 and c == n - 1:
-                    placed_queens = []
-                    stop = True
-                r -= 1
-                c += 1
-            else:
-                c = 0
-            break
-        if stop:
-            break
-        # on fail: go back to previous row
-        # and continue from last safe column + 1
-        if goback:
-            r -= 1
-            while r >= 0:
-                c = placed_queens[r][1] + 1
-                del placed_queens[r]  # delete previous queen coordinates
-                if c < n:
-                    break
-                r -= 1
-            if r < 0:
-                break
-            continue
-        r += 1
 
-    for idx, val in enumerate(solutions):
-        if idx == len(solutions) - 1:
-            print(val, end='')
-        else:
-            print(val)
+# Define the backtracking function to solve N Queens
+def solve_nqueens(N):
+    """Solve the N Queens problem and print all solutions"""
+    def backtrack(row, board):
+        if row == N:
+            # If all queens are placed, print the board configuration
+            print([[i, board[i]] for i in range(N)])
+            return
+
+        for col in range(N):
+            if is_safe(board, row, col):
+                board[row] = col
+                backtrack(row + 1, board)
+                board[row] = -1
+
+    board = [-1] * N
+    backtrack(0, board)
+
+
+# Call the solver
+solve_nqueens(N)
